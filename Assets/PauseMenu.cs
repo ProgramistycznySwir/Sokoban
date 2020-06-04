3288a4403a12b345103cec5559ea799220b97693
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2020-06-04 22:25:52 +0200

    new class
    Wszystkie podstawowe funkcjonalnosci z uml'a: kod + dzialajace przyciski

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI; 
+    void Start()
+    {
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+    public void Exit() //wywala cie do glownego Menu
+    {
+        //Mozna dac jakas stringa zamiast "Menu", ale nie chce jeszcze bardziej zmieniac umla 
+        SceneManager.LoadScene("Menu");
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // wczytuje aktywna scene
+        Time.timeScale = 1f;                                        //must have, inaczej gra sie pauzuje
+    }
+}
