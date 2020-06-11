396a75daee94c8bd884b881e3f86471fbd2775c9
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2020-06-11 17:04:52 +0200

    Update CameraTest.cs

--- Assets/Editor/CameraTest.cs
+++ Assets/Editor/CameraTest.cs
@@ -12,15 +12,15 @@ namespace Tests
         [Test]
         public void CameraPositionTesting()
         {
-            // Inicjowanie obiektów i ustawianie wartości oczekiwanej
+            // Inicjowanie obiektów i ustawienie wartości oczekiwanej
             Vector2Int mapSize = new Vector2Int(10,10);
             Level level = new Level();
             Vector3 expected = new Vector3(5f, 10.3923054f, -5f);
             
-            // Wywolanie sprawdzanej metody
+            // Wywołanie sprawdzanej metody
             Vector3 actual = level.PlaceCamera(mapSize);
             
-            // Porownywanie wartosci oczekiwanych z otrzymanymi
+            // Porównywanie wartości oczekiwanych z otrzymanymi
             Assert.AreEqual(expected[0], actual[0]);
             Assert.AreEqual(expected[1], actual[1]);
             Assert.AreEqual(expected[2], actual[2]);
